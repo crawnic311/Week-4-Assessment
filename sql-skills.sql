30a9f33d44813f4d1c452c1f4b4a04eed6bcd59a
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-08 09:43:15 -0600

    Employee Table Completed

--- sql-skills.sql
+++ sql-skills.sql
@@ -12,3 +12,19 @@ SELECT * FROM artist
 ORDER BY name ASC 
 LIMIT 5
 
+
+--Employee Table
+--Problem 1
+SELECT first_name, last_name FROM employee
+WHERE city = 'Calgary'
+
+
+--Problem 2
+--Nancy Edwards employee_id is 2
+SELECT * FROM employee
+WHERE reports_to = 2
+
+
+--Problem 3
+SELECT COUNT(*) FROM employee
+WHERE city = 'Lethbridge'
