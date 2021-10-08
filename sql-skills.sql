0b2b026a3892cc31a9a4dff82d4ef4269b7a5083
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-08 09:37:47 -0600

    Artists Table Completed

---
+++ sql-skills.sql
@@ -0,0 +1,14 @@
+--Artist Table
+--Problem 1
+INSERT INTO artist(name)
+VALUES
+  ('Ruston Kelly'),
+  ('311'),
+  ('Penny and Sparrow')
+
+
+--Problem 2
+SELECT * FROM artist
+ORDER BY name ASC 
+LIMIT 5
+
