39d1bdb80acf32d6e1e5157ff6637aae0e1a3c0c
DILLON CRAW <68667382+crawnic311@users.noreply.github.com>
2021-10-08 10:02:21 -0600

    Invoice Table Completed

--- sql-skills.sql
+++ sql-skills.sql
@@ -28,3 +28,36 @@ WHERE reports_to = 2
 --Problem 3
 SELECT COUNT(*) FROM employee
 WHERE city = 'Lethbridge'
+
+
+--Invoice Table
+--Problem 1
+SELECT COUNT(*) FROM invoice
+WHERE billing_country = 'USA'
+
+
+--Problem 2
+SELECT MAX(total) FROM invoice
+
+
+--Problem 3
+SELECT MIN(total) FROM invoice
+
+
+--Problem 4
+SELECT i.invoice_id FROM invoice i
+JOIN invoice_line il ON il.invoice_id = i.invoice_id
+WHERE (il.unit_price * i.total) > 5
+
+
+--Problem 5
+SELECT COUNT(DISTINCT i.invoice_id) FROM invoice i
+JOIN invoice_line il ON il.invoice_id = i.invoice_id
+WHERE (il.unit_price * i.total) < 5 --228 orders
+
+
+--Problem 6
+SELECT SUM(total) FROM invoice
+
+
+
